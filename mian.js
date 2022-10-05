d2cf7b1dc88653661a3b4f3c7dad25f471999988
Ahmed-Samir444 <88825424+Ahmed-Samir444@users.noreply.github.com>
2022-10-05 15:56:11 +0200

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,11 @@
+// get toggle
+let toggle = document.querySelector('.toggle')
+
+// get nav-list
+let navlist = document.querySelector('.nav-list')
+
+// show the nav 
+toggle.addEventListener('click', function () {
+    navlist.style.display = 'block'
+})
+
