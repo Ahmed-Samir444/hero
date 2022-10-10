3fd336447fd4b2cdd472479f760cb7ceeb6d2f10
Ahmed-Samir444 <88825424+Ahmed-Samir444@users.noreply.github.com>
2022-10-10 18:16:04 +0200

    Add files via upload

--- js/mian.js
+++ js/mian.js
@@ -4,8 +4,16 @@ let toggle = document.querySelector('.toggle')
 // get nav-list
 let navlist = document.querySelector('.nav-list')
 
+// get close
+let closer = document.querySelector('.close')
+
 // show the nav 
 toggle.addEventListener('click', function () {
-    navlist.style.display = 'block'
+    navlist.classList.add('open')
+})
+
+// hide the nav
+closer.addEventListener('click', function () {
+    this.parentElement.classList.remove('open')
 })
 
